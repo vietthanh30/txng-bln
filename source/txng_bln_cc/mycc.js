0135208219562ba4fa8b3a296215f09f43d51a84
hainv <hai213k57@gmail.com>
2022-02-26 12:14:29 +0700

    Code
    Signed-off-by: hainv <hai213k57@gmail.com>

--- source/txng_bln_cc/mycc.js
+++ source/txng_bln_cc/mycc.js
@@ -21,7 +21,7 @@ class TxngBlnContract extends Contract {
   async addBlock(ctx, blockId, blockData) {
     // await ctx.stub.putState(studentId, Buffer.from(JSON.stringify(marks)));
     try {
-      await ctx.stub.putState(blockId, Buffer.from(blockData.toString()));
+      await ctx.stub.putState(blockId, Buffer.from(blockData));
       console.log("Block data added to the ledger succesfully");
     } catch (err) {
       console.log("Add block error: ", err);
